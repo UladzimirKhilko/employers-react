300de164bf67e7a66bca061bb3edb43a22f569da
Vladimir Khilko <vladimirhilko@gmail.com>
2024-11-25 18:45:50 +0300

    progect-react employers

---
+++ src/components/search-panel/search-panel.js
@@ -0,0 +1,13 @@
+/* eslint-disable react/react-in-jsx-scope */
+import './search-panel.css'
+
+const SearchPanel = () => {
+    return (
+       <input 
+       type="text"
+       className="form-control search-input"
+       placeholder="Найти сотрудника"/> 
+    )
+}
+
+export default SearchPanel;
