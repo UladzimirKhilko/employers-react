fd6310380808528ffebabeff39a402315e5e4434
Vladimir Khilko <vladimirhilko@gmail.com>
2024-11-27 20:46:27 +0300

    refactoring EmployersListItem to class

--- src/components/employers-list-item/employers-list-item.js
+++ src/components/employers-list-item/employers-list-item.js
@@ -1,32 +1,64 @@
+/* eslint-disable no-unused-labels */
 /* eslint-disable react/prop-types */
 /* eslint-disable react/react-in-jsx-scope */
+import { Component } from 'react';
+
 import './employers-list-item.css'
 
-const EmployersListItem = ({name, salary, increase}) => {
+class EmployersListItem extends Component {
 
-    let className = "list-group-item d-flex justify-content-between"
-    if (increase) {
-        className += ' increase';
+    constructor(props) {
+        super(props);
+        this.state = {
+            increase: false,
+            like: false
+        }
     }
 
-    return (
-        <li className={className}>
-            <span className="list-group-item-label">{name}</span>
-            <input type="text" className="list-group-item-input" defaultValue={salary + '$'}/>
-            <div className='d-flex justify-content-center align-items-center'>
-                <button type="button"
-                    className="btn-cookie btn-sm ">
-                    <i className="fas fa-cookie"></i>
-                </button>
+    onIncrease = () => {
+        this.setState(({increase}) => ({
+            increase: !increase
+        }))
+    }
+    onLike = () => {
+        this.setState(({like}) => ({
+            like: !like
+        }))
+    }
 
-                <button type="button"
-                        className="btn-trash btn-sm ">
-                    <i className="fas fa-trash"></i>
-                </button>
-                <i className="fas fa-star"></i>
-            </div>
-        </li>
-    )
+    render () {
+        const {name, salary} = this.props;
+        const {increase, like} = this.state;
+
+        let className = "list-group-item d-flex justify-content-between"
+        if (increase) {
+            className += ' increase';
+        }
+
+        if (like) {
+            className += ' like';
+        }
+
+        return (
+            <li className={className}>
+                <span onClick={this.onLike} className="list-group-item-label">{name}</span>
+                <input type="text" className="list-group-item-input" defaultValue={salary + '$'}/>
+                <div className='d-flex justify-content-center align-items-center'>
+                    <button type="button"
+                        className="btn-cookie btn-sm "
+                        onClick={this.onIncrease}>
+                        <i className="fas fa-cookie"></i>
+                    </button>
+
+                    <button type="button"
+                            className="btn-trash btn-sm ">
+                        <i className="fas fa-trash"></i>
+                    </button>
+                    <i className="fas fa-star"></i>
+                </div>
+            </li>
+        )
+    }
 }
 
 export default EmployersListItem;
